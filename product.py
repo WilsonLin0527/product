19a8d014c71e46c8f52eec36dc342be6ffb0be10
Wilson Lin <sw147852369@hotmail.com>
2018-07-17 15:55:35 +0800

    add function

--- product.py
+++ product.py
@@ -4,8 +4,14 @@ while True:
 	if 'q' == name:
 		break;
 	price = input('insert price:');
-	p = [];
-	p.append(name);
-	p.append(price)
+	p = [name, price];
 	product.append(p);
 print(product);
+
+for p in product:
+	print(p[0], 'is $', p[1]);
+
+with open('product.csv', 'w', encoding = 'utf-8') as f:
+	f.write('商品,價格 \n');
+	for p in product:
+		f.write(p[0] + ',' + p[1] + '\n')
